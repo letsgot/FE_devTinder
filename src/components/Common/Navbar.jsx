789ee84ec4a7d8b41572c0d128c2de071181f9d3
Vivek Chauhan <Vivek.c@radiantinfonet.com>
2025-06-15 14:21:23 +0530

    last commit for logout

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -25,13 +25,13 @@ const Navbar = () => {
                 {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
                 <div className="flex items-center">Welcome {userDetail?.firstName}</div>
                 <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                    {userDetail?.photoUrl ? <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
                                 alt="Tailwind CSS Navbar component"
                                 src={userDetail?.photoUrl} />
                         </div>
-                    </div>
+                    </div> : <></>}
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
